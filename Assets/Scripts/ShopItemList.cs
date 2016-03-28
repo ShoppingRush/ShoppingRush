2d8bb69a64f5e37994b59fb3b89fb822de427e08
akub Łabuz <jakub.k.labuz@gmail.com>
2016-03-28 14:02:41 +0200

    [SR-7] Show simple list

---
+++ Assets/Scripts/ShopItemList.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Inventory))]
+    public class ShopItemList : MonoBehaviour
+    {
+        private Dictionary<ShopItemData, int> _shopItemToCollect;
+
+        public Dictionary<ShopItemData, int> ShopItemToCollect {
+            get
+            {
+                return _shopItemToCollect;
+            }
+            set
+            {
+                _shopItemToCollect = value;
+                RedrawUI();
+            }
+        }
+
+        public Text ShopItemListText;
+
+        private Inventory _inventory;
+
+        // Use this for initialization
+        void Start()
+        {
+            _shopItemToCollect = new Dictionary<ShopItemData, int>()
+            {
+                {new ShopItemData { ItemId = 0, ItemName = "Mleko" }, 10 },
+                {new ShopItemData { ItemId = 1, ItemName = "Woda" }, 10 },
+                {new ShopItemData { ItemId = 2, ItemName = "Sok" }, 10 },
+                {new ShopItemData { ItemId = 3, ItemName = "Chleb" }, 10 },
+                {new ShopItemData { ItemId = 4, ItemName = "Mleko" }, 10 }
+            };
+
+
+            _inventory = GetComponent<Inventory>();
+            ShopItemListText.supportRichText = true;
+            RedrawUI();
+        }
+
+        public void RedrawUI()
+        {
+            if (ShopItemListText != null)
+            {
+                ShopItemListText.text = String.Join("\n\r", ShopItemToCollect.Keys.
+                    Select(item => getStringForItem(item, _inventory.GetAmountOfShopItemData(item))).ToArray<string>());
+            }
+            else
+            {
+                ShopItemListText.text = "";
+            }
+        }
+
+        private string getStringForItem(ShopItemData item, int currentAmount)
+        {
+            return String.Format(getFormat(ShopItemToCollect[item], currentAmount), String.Format("{0}x {1}", ShopItemToCollect[item], item.ItemName));
+        }
+
+        private string getFormat(int number, int currentAmount)
+        {
+            return currentAmount >= number ? "<strike>{0}</strike>" : "{0}";
+        }
+    }
+}
---
+++ Assets/Scripts/StrikethroughText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Image))]
+    public class StrikethroughText : MonoBehaviour
+    {
+        public Text Text;
+
+        public bool Strikethrough;
+
+        private Image _image;
+
+        // Use this for initialization
+        void Start()
+        {
+            _image = GetComponent<Image>();
+            _image.rectTransform.sizeDelta = new Vector2(Text.preferredWidth, 3);
+            _image.rectTransform.position = new Vector3(0, Text.preferredHeight);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnGUI()
+        {
+            _image.enabled = Strikethrough;
+        }
+    }
+}
